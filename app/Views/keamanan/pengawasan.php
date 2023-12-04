41fc43cb41c477fd0b6ec8016dfb08ead8abcd97
Didah Siti Nursaadah <didahstnursaadah@gmail.com>
2023-12-04 08:35:47 +0700

    Update pengawasan.php
    change title modal edit pengawasan

--- app/Views/keamanan/pengawasan.php
+++ app/Views/keamanan/pengawasan.php
@@ -135,7 +135,7 @@
                         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h6 class="modal-title" id="exampleModalLongTitle">Edit Pelaporan Kejadian</h6>
+                                    <h6 class="modal-title" id="exampleModalLongTitle">Edit Pengawasan Personil</h6>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
